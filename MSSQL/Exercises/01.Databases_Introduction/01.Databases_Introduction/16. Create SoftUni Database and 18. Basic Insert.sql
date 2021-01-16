6be5d4b86b69f364cb663a5119236be5567ba378
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-01-16 21:51:41 +0200

    Add done 16. Create SoftUni Database and 18. Basic Insert tasks

---
+++ MSSQL/Exercises/01.Databases_Introduction/01.Databases_Introduction/16. Create SoftUni Database and 18. Basic Insert.sql
@@ -0,0 +1,51 @@
+CREATE TABLE Towns 
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(100) NOT NULL
+)
+
+CREATE TABLE Addresses 
+(
+	Id INT PRIMARY KEY IDENTITY,
+	AddressText NVARCHAR(MAX) NOT NULL
+)
+
+CREATE TABLE Departments  
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(100) NOT NULL
+)
+
+CREATE TABLE Employees 
+(
+	Id INT PRIMARY KEY IDENTITY,
+	FirstName NVARCHAR(100) NOT NULL,
+	MiddleName NVARCHAR(100) NOT NULL,
+	LastName NVARCHAR(100) NOT NULL,
+	JobTitle NVARCHAR(100) NOT NULL,
+	DepartmentId INT FOREIGN KEY REFERENCES Departments(Id),
+	HireDate NVARCHAR(20) NOT NULL,
+	Salary DECIMAL NOT NULL,
+	AddressId INT FOREIGN KEY REFERENCES Addresses(Id)
+)
+
+INSERT INTO Towns ([Name]) VALUES
+('Sofia'),
+('Plovdiv'),
+('Varna'),
+('Burgas')
+
+INSERT INTO Departments ([Name]) VALUES
+('Engineering'),
+('Sales'),
+('Marketing'),
+('Software Development'),
+('Quality Assurance')
+
+INSERT INTO Employees
+(FirstName, MiddleName, LastName, JobTitle, DepartmentId, HireDate, Salary) VALUES
+('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '01/02/2013', 3500.00),
+('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '02/03/2004', 4000.00),
+('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '28/08/2016', 525.25),
+('Georgi', 'Teziev', 'Ivanov', 'CEO', 2, '09/12/2007', 3000.00),
+('Peter', 'Pan', 'Pan', 'Intern', 3, '28/08/2016', 599.88)
