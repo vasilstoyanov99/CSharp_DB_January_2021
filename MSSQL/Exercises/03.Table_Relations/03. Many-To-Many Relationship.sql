8486a08d7c751655ebeaa4504c3775fa463e6531
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-01-20 02:00:51 +0200

    Add done 03. Many-To-Many Relationship task

---
+++ MSSQL/Exercises/03.Table_Relations/03. Many-To-Many Relationship.sql
@@ -0,0 +1,38 @@
+CREATE TABLE Students
+(
+	StudentID INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(60)
+)
+
+CREATE TABLE Exams
+(
+	ExamID INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] NVARCHAR(100)
+)
+
+CREATE TABLE StudentsExams
+(
+	StudentID INT NOT NULL, 
+	ExamID INT NOT NULL
+)
+
+ALTER TABLE StudentsExams
+ADD CONSTRAINT CK_StudentID_ExamID PRIMARY KEY (StudentID, ExamID)
+
+INSERT INTO Students ([Name]) VALUES
+('Mila'),
+('Toni'),
+('Ron')
+
+INSERT INTO Exams ([Name]) VALUES
+('SpringMVC'),
+('Neo4j'),
+('Oracle 11g')
+
+INSERT INTO StudentsExams ([StudentID], [ExamID]) VALUES
+(1, 101),
+(1, 102),
+(2, 101),
+(3, 103),
+(2, 102),
+(2, 103)
