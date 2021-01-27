9c21c68c13219bb7b3e0446ad07a5036615d00dc
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-01-27 03:42:32 +0200

    Add done 10. Employees Summary task

---
+++ MSSQL/Exercises/05.Subqueries_and_Joins/10. Employees Summary.sql
@@ -0,0 +1,8 @@
+SELECT TOP(50) e1.EmployeeID,
+e1.FirstName + ' ' + e1.LastName AS 'EmployeeName',
+e2.FirstName + ' ' + e2.LastName AS 'ManagerName',
+d.[Name] AS 'DepartmentName'
+FROM Employees AS e1
+JOIN Employees AS e2 ON e1.ManagerID = e2.EmployeeID
+JOIN Departments AS d ON e1.DepartmentID = d.DepartmentID
+ORDER BY e1.EmployeeID
