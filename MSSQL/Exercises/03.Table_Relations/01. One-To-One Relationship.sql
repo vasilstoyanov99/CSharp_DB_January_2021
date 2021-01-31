4410d0e5f2c2eae7e1fa7c3a49c4e3028f43bb9c
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-01-31 23:56:51 +0200

    Update 01. One-To-One Relationship.sql

--- MSSQL/Exercises/03.Table_Relations/01. One-To-One Relationship.sql
+++ MSSQL/Exercises/03.Table_Relations/01. One-To-One Relationship.sql
@@ -1,24 +1,16 @@
-CREATE TABLE Persons
-(
-	PersonID INT NOT NULL,
-	FirstName NVARCHAR(60) NOT NULL,
-	Salary DECIMAL(7, 2) NOT NULL,
-)
-
 CREATE TABLE Passports
 (
 	PassportID INT PRIMARY KEY IDENTITY(101, 1),
 	PassportNumber NVARCHAR(MAX)
 )
 
-ALTER TABLE Persons
-ADD PassportID INT 
-
-ALTER TABLE Persons 
-ADD CONSTRAINT PK_PersonID PRIMARY KEY (PersonID)
-
-ALTER TABLE Persons
-ADD FOREIGN KEY (PassportID) REFERENCES Passports(PassportID)
+CREATE TABLE Persons
+(
+	PersonID INT PRIMARY KEY (PersonID),
+	FirstName NVARCHAR(60) NOT NULL,
+	Salary DECIMAL(7, 2) NOT NULL,
+	PassportID INT UNIQUE FOREIGN KEY REFERENCES Passports(PassportID)
+)
 
 INSERT INTO Passports (PassportNumber) VALUES
 ('N34FG21B'),
