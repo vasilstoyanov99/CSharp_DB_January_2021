058cc37f9e8249a36251a38676343a19ad74cbb4
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-02-02 17:27:56 +0200

    Add done 10. People with Balance Higher Than task

---
+++ MSSQL/Exercises/07.Functions_and_Stored_Procedures/10. People with Balance Higher Than.sql
@@ -0,0 +1,16 @@
+CREATE PROC usp_GetHoldersWithBalanceHigherThan 
+		(@Money DECIMAL(18, 4))
+ AS
+	BEGIN
+		SELECT FirstName AS 'First Name', 
+			 LastName AS 'Last Name' 
+			FROM Accounts AS a
+				JOIN AccountHolders AS ah ON a.AccountHolderId = ah.Id
+			GROUP BY FirstName, LastName
+			HAVING SUM(Balance) > @Money
+			ORDER BY FirstName, LastName
+	END
+
+GO
+
+EXEC usp_GetHoldersWithBalanceHigherThan 500000
