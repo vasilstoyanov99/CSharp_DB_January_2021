e498a2b18e9298f71daf153b2decd75c53408057
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-02-08 12:29:16 +0200

    Add done 06. Select All Pilots task

---
+++ MSSQL/Exam_Preparation/28_Jun_2020/06. Select All Pilots.sql
@@ -0,0 +1,5 @@
+SELECT c.Id AS 'id', c.FirstName + ' ' + c.LastName AS 'full_name'
+FROM Colonists AS c
+	JOIN TravelCards AS tc ON c.Id = tc.ColonistId
+WHERE tc.JobDuringJourney = 'Pilot'
+ORDER BY c.Id ASC
