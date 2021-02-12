ed354e9ff2f6d0ce12f2b3843a075c3623ccb114
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-02-12 20:49:19 +0200

    Update 11. Count of Countries by Currency.sql

--- MSSQL/Exercises/09. Additional Exercises/11. Count of Countries by Currency.sql
+++ MSSQL/Exercises/09. Additional Exercises/11. Count of Countries by Currency.sql
@@ -1,7 +1,7 @@
 SELECT cr.CurrencyCode AS 'CurrencyCode', 
 	   cr.[Description] AS 'Currency', 
-	   COUNT(c.CountryName) AS 'NumberOfCountries'
-FROM Countries AS c
-	LEFT JOIN Currencies AS cr ON cr.CurrencyCode = c.CurrencyCode
+	   COUNT(c.CountryCode) AS 'NumberOfCountries'
+FROM Currencies AS cr 
+	LEFT JOIN Countries AS c ON cr.CurrencyCode = c.CurrencyCode
 GROUP BY cr.CurrencyCode, cr.[Description]
 ORDER BY 'NumberOfCountries' DESC, cr.[Description] ASC
