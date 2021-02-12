089d6464b420a52d19b69c2bef4180d0a705fe60
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-02-12 17:58:00 +0200

    Add done 06. Display All Items about Forbidden Game Type task

---
+++ MSSQL/Exercises/09. Additional Exercises/06. Display All Items about Forbidden Game Type.sql
@@ -0,0 +1,8 @@
+SELECT i.[Name] AS 'Item', 
+	   i.Price, 
+	   i.MinLevel, 
+	   gt.[Name] AS 'Forbidden Game Type'
+FROM Items AS i
+	LEFT JOIN GameTypeForbiddenItems AS fi ON fi.ItemId = i.Id
+	LEFT JOIN GameTypes AS gt ON fi.GameTypeId = gt.Id
+ORDER BY 'Forbidden Game Type' DESC, 'Item' ASC
