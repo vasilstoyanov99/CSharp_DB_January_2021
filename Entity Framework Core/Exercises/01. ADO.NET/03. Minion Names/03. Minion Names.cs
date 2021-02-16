e50f8c7d2a409d8a3a7bd7d821eb01c698f27f4c
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-02-16 17:46:00 +0200

    Add done 03. Minion Names task

---
+++ Entity Framework Core/Exercises/01. ADO.NET/03. Minion Names/03. Minion Names.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace _03._Minion_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection("Server=.;Integrated Security=true;Database=MinionsDB"))
+            {
+                connection.Open();
+                Console.WriteLine("Enter villain Id: ");
+                int villainId = int.Parse(Console.ReadLine());
+
+                string queryToFindVillainName = $"SELECT [Name] FROM Villains WHERE Id = {villainId}";
+                SqlCommand command = new SqlCommand(queryToFindVillainName, connection);
+
+                if (object.Equals(command.ExecuteScalar(), null))
+                {
+                    Console.WriteLine($"No villain with ID <{villainId}> exists in the database.");
+                    return;
+                }
+
+                string queryToCheckMinionsCount = $"SELECT COUNT(*)" +
+                                                  $"FROM MinionsVillains AS mv" +
+                                                  $"	JOIN Minions As m ON mv.MinionId = m.Id" +
+                                                  $"WHERE mv.VillainId = {villainId}";
+                command = new SqlCommand(queryToCheckMinionsCount, connection);
+                command = new SqlCommand(queryToFindVillainName, connection);
+                string villainName = (string)command.ExecuteScalar();
+
+                if (object.Equals(command.ExecuteScalar(), null))
+                {
+                    Console.WriteLine($"Villain: {villainName}");
+                    Console.WriteLine("(no minions)");
+                    return;
+                }
+
+                string queryToGetAllMinions = 
+                    "SELECT ROW_NUMBER() OVER (ORDER BY m.[Name]) as RowNum," +
+                    "m.[Name]," +
+                    "m.Age" +
+                    " FROM MinionsVillains AS mv" +
+                    "  JOIN Minions As m ON mv.MinionId = m.Id" +
+                    $"WHERE mv.VillainId = {villainId}" +
+                    "ORDER BY m.[Name]";
+                command = new SqlCommand(queryToGetAllMinions, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                Console.WriteLine($"Villain: {villainName}");
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader["RowNum"]}. {reader["[Name]"]} {reader["Age"]}");
+                }
+            }
+        }
+    }
+}
