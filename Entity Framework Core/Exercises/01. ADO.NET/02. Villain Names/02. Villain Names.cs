eb6a85ac3090de3d34a233211268d80e1463c1b1
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-02-16 17:02:18 +0200

    Add done 02. Villain Names task

---
+++ Entity Framework Core/Exercises/01. ADO.NET/02. Villain Names/02. Villain Names.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace _02._Villain_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection("Server=.;Integrated Security=true;Database=MinionsDB"))
+            {
+                connection.Open();
+                string query = "SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  " +
+                               "FROM Villains AS v " +
+                               "    JOIN MinionsVillains AS mv ON v.Id = mv.VillainId " +
+                               "GROUP BY v.Id, v.Name " +
+                               "  HAVING COUNT(mv.VillainId) > 3 " +
+                               "ORDER BY COUNT(mv.VillainId)";
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader[0]} - {reader[1]}");
+                }
+            }
+        }
+    }
+}
