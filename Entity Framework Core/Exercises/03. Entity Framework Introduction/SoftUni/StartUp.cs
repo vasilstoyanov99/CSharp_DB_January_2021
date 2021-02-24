4c22a7249dd6e7be59ab615cf322a5a67f7667d2
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-02-24 19:29:41 +0200

    Add done 07. Employees and Projects task

--- Entity Framework Core/Exercises/03. Entity Framework Introduction/SoftUni/StartUp.cs
+++ Entity Framework Core/Exercises/03. Entity Framework Introduction/SoftUni/StartUp.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using Microsoft.Data.SqlClient.Server;
+using Microsoft.EntityFrameworkCore;
 using SoftUni.Data;
 using SoftUni.Models;
 
@@ -11,7 +15,7 @@ namespace SoftUni
         static void Main(string[] args)
         {
             SoftUniContext context = new SoftUniContext();
-            Console.WriteLine(AddNewAddressToEmployee(context));
+            Console.WriteLine(GetEmployeesInPeriod(context));
         }
 
         //public static string GetEmployeesFullInformation(SoftUniContext context)
@@ -86,40 +90,88 @@ namespace SoftUni
         //    return result.ToString().TrimEnd();
         //}
 
-        public static string AddNewAddressToEmployee(SoftUniContext context)
+        //public static string AddNewAddressToEmployee(SoftUniContext context)
+        //{
+        //    var address = new Address
+        //    {
+        //        AddressText = "Vitoshka 15",
+        //        TownId = 4
+        //    };
+
+        //    var employee = context
+        //        .Employees
+        //        .FirstOrDefault(x => x.LastName == "Nakov");
+        //    address.Employees.Add(employee);
+        //    context.Addresses.Add(address);
+        //    context.SaveChanges();
+        //    employee.AddressId = null;
+        //    employee.AddressId = address.AddressId;
+        //    context.SaveChanges();
+
+        //    var employeesAddressText = context
+        //        .Employees
+        //        .Select(x => new
+        //        {
+        //            x.Address.AddressText,
+        //            x.Address.AddressId
+        //        })
+        //        .OrderByDescending(x => x.AddressId)
+        //        .Take(10)
+        //        .ToList();
+
+        //    StringBuilder result = new StringBuilder();
+
+        //    foreach (var e in employeesAddressText)
+        //    {
+        //        result.AppendLine(e.AddressText);
+        //    }
+
+        //    return result.ToString().TrimEnd();
+        //}
+
+        public static string GetEmployeesInPeriod(SoftUniContext context)
         {
-            var address = new Address
-            {
-                AddressText = "Vitoshka 15",
-                TownId = 4
-            };
-
-            var employee = context
-                .Employees
-                .FirstOrDefault(x => x.LastName == "Nakov");
-            address.Employees.Add(employee);
-            context.Addresses.Add(address);
-            context.SaveChanges();
-            employee.AddressId = null;
-            employee.AddressId = address.AddressId;
-            context.SaveChanges();
-
-            var employeesAddressText = context
-                .Employees
+            var sortedEmployees = context
+                .Employees.Include(x => x.EmployeesProjects)
+                .ThenInclude(x => x.Project)
+                .Where(x =>
+                    x.EmployeesProjects
+                        .Any(p =>
+                            p.Project.StartDate.Year >= 2001 && p.Project.StartDate.Year <= 2003))
                 .Select(x => new
                 {
-                    x.Address.AddressText,
-                    x.Address.AddressId
+                    EmployeeFirstName = x.FirstName,
+                    EmployeeLastName = x.LastName,
+                    ManagerFirstName = x.Manager.FirstName,
+                    ManagerLastName = x.Manager.LastName,
+                    Projects = x.EmployeesProjects.Select(p => new
+                    {
+                        ProjectName = p.Project.Name,
+                        StartDate = p.Project.StartDate,
+                        EndDate = p.Project.EndDate
+                    })
                 })
-                .OrderByDescending(x => x.AddressId)
                 .Take(10)
                 .ToList();
 
-            StringBuilder result = new StringBuilder();
+            var result = new StringBuilder();
 
-            foreach (var e in employeesAddressText)
+            foreach (var e in sortedEmployees)
             {
-                result.AppendLine(e.AddressText);
+                result.AppendLine($"{e.EmployeeFirstName} {e.EmployeeLastName} - " +
+                                  $"Manager: {e.ManagerFirstName} {e.ManagerLastName}");
+
+                foreach (var p in e.Projects)
+                {
+                    var endDate = p.EndDate.HasValue 
+                        ? p.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture) 
+                        : "not finished";
+
+                    var startDate = p.StartDate
+                        .ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture);
+
+                    result.AppendLine($"--{p.ProjectName} - {startDate} - {endDate}");
+                }
             }
 
             return result.ToString().TrimEnd();
