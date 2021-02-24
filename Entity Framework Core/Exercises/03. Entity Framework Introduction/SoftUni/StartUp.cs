7b3d69a56acbc752d1327069c6ae02de2c40815e
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-02-24 16:15:34 +0200

    Add done 04. Employees with Salary Over 50 000 task

--- Entity Framework Core/Exercises/03. Entity Framework Introduction/SoftUni/StartUp.cs
+++ Entity Framework Core/Exercises/03. Entity Framework Introduction/SoftUni/StartUp.cs
@@ -10,19 +10,35 @@ namespace SoftUni
         static void Main(string[] args)
         {
             SoftUniContext context = new SoftUniContext();
-            Console.WriteLine(GetEmployeesFullInformation(context));
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
         }
 
         public static string GetEmployeesFullInformation(SoftUniContext context)
         {
             var employees = context.Employees.
-                OrderBy(e => e.EmployeeId);
+                OrderBy(e => e.EmployeeId).ToList();
             StringBuilder result = new StringBuilder();
 
             foreach (var e in employees)
             {
                 result.AppendLine($"{e.FirstName} {e.LastName} " +
                                   $"{e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+            }
+
+            return result.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var sortedEmployees = context.Employees.
+                Where(x => x.Salary > 50000).
+                OrderBy(x => x.FirstName).ToList();
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (var e in sortedEmployees)
+            {
+                result.AppendLine($"{e.FirstName} - {e.Salary:f2}");
             }
 
             return result.ToString().TrimEnd();
