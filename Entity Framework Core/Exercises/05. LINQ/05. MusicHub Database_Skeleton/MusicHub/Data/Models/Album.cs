74265eac60011fe07b76cb3fd9c05ab3216fbb0e
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-03-03 17:10:34 +0200

    Update Album.cs

--- Entity Framework Core/Exercises/05. LINQ/05. MusicHub Database_Skeleton/MusicHub/Data/Models/Album.cs
+++ Entity Framework Core/Exercises/05. LINQ/05. MusicHub Database_Skeleton/MusicHub/Data/Models/Album.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace MusicHub.Data.Models
