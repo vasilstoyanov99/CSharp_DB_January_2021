5e86b873442ad07ce951368b8796df3f99f39c3c
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-03-05 12:21:37 +0200

    Add Advanced Querying exercise skeleton

---
+++ Entity Framework Core/Exercises/06. Advanced-Querying-BookShop/BookShop/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using BookShop.Models.Enums;
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+           
+        }
+
+        
+    }
+}
