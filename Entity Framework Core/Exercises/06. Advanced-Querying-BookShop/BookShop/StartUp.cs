58228ddf5f46567a218cc1d01a043ee67a51520f
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-03-05 12:35:34 +0200

    Add done 1. Age Restriction tasks

--- Entity Framework Core/Exercises/06. Advanced-Querying-BookShop/BookShop/StartUp.cs
+++ Entity Framework Core/Exercises/06. Advanced-Querying-BookShop/BookShop/StartUp.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Text;
+
 using BookShop.Models.Enums;
 
 namespace BookShop
@@ -14,9 +15,34 @@ namespace BookShop
         {
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
-           
+            string ageRestriction = Console.ReadLine();
+            Console.WriteLine(GetBooksByAgeRestriction(db, ageRestriction));
         }
 
-        
+        public static string GetBooksByAgeRestriction
+            (BookShopContext context, string command)
+        {
+            AgeRestriction ageRestrictionEnum = Enum
+                .Parse<AgeRestriction>(command, true);
+
+            var sortedTitles = context
+                .Books
+                .Where(x => x.AgeRestriction == ageRestrictionEnum)
+                .Select(x => new
+                {
+                    x.Title
+                })
+                .OrderBy(x => x.Title)
+                .ToList();
+
+            var result = new StringBuilder();
+
+            foreach (var book in sortedTitles)
+            {
+                result.AppendLine(book.Title);
+            }
+
+            return result.ToString().Trim();
+        }
     }
 }
