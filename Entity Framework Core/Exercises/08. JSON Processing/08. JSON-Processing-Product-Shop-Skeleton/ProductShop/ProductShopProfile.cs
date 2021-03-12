922ee0b62b9e7fa2305641fdefb274e3a151ecbf
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-03-12 15:44:23 +0200

    Add done 05. Export Products In Range task

--- Entity Framework Core/Exercises/08. JSON Processing/08. JSON-Processing-Product-Shop-Skeleton/ProductShop/ProductShopProfile.cs
+++ Entity Framework Core/Exercises/08. JSON Processing/08. JSON-Processing-Product-Shop-Skeleton/ProductShop/ProductShopProfile.cs
@@ -1,11 +1,16 @@
 ﻿using AutoMapper;
 
+using ProductShop.Data.DTO.ProductsDTO;
+using ProductShop.Models;
+
 namespace ProductShop
 {
     public class ProductShopProfile : Profile
     {
         public ProductShopProfile()
         {
+            CreateMap<Product, ProductsInRange>()
+                .ForMember(x => x.Seller, y => y.MapFrom(s => s.Seller.FirstName + " " + s.Seller.LastName));
         }
     }
 }
--- Entity Framework Core/Exercises/08. JSON Processing/08. JSON-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
+++ Entity Framework Core/Exercises/08. JSON Processing/08. JSON-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
@@ -1,29 +1,53 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using System.Linq;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Newtonsoft.Json;
 
 using ProductShop.Data;
-using ProductShop.Models;
+using ProductShop.Data.DTO.ProductsDTO;
 
 namespace ProductShop
 {
     public class StartUp
     {
+        private static string OutputPath = "../../../Datasets/Output";
+        
         public static void Main(string[] args)
         {
+            InitializeMapper();
             var context = new ProductShopContext();
-            string json = File.ReadAllText("../../../Datasets/categories-products.json");
-            Console.WriteLine(ImportCategoryProducts(context, json));
+            var json = GetProductsInRange(context);
+            EnsureDirectoryExists();
+            File.WriteAllText(OutputPath + "/products-in-range.json", json);
         }
 
-        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        public static string GetProductsInRange(ProductShopContext context)
         {
-            var categoriesProducts = JsonConvert
-                .DeserializeObject<CategoryProduct[]>(inputJson);
-            context.CategoryProducts.AddRange(categoriesProducts);
-            context.SaveChanges();
-            var result = $"Successfully imported {categoriesProducts.Length}";
-            return result;
+            var sortedProducts = context
+                .Products
+                .Where(x => x.Price >= 500 && x.Price <= 1000)
+                .OrderBy(x => x.Price)
+                .ProjectTo<ProductsInRange>()
+                .ToList();
+           var json = JsonConvert.SerializeObject(sortedProducts, Formatting.Indented);
+           return json;
+        }
+
+        private static void EnsureDirectoryExists()
+        {
+            if (!Directory.Exists(OutputPath))
+            {
+                Directory.CreateDirectory(OutputPath);
+            }
+        }
+
+         private static void InitializeMapper()
+        {
+            Mapper.Initialize(config =>
+            {
+                config.AddProfile<ProductShopProfile>();
+            });
         }
     }
 }
