c0738b62c9d8373fd73e13f02ef3a64be1445a66
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-03-14 19:01:47 +0200

    Add done 09. Import Suppliers task

--- Entity Framework Core/Exercises/08. JSON Processing/08. JSON-Processing-Car-Dealer-Skeleton/CarDealer/StartUp.cs
+++ Entity Framework Core/Exercises/08. JSON Processing/08. JSON-Processing-Car-Dealer-Skeleton/CarDealer/StartUp.cs
@@ -17,32 +17,39 @@ namespace CarDealer
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
 
-            //Insert Suppliers Data 
+            ////Insert Suppliers Data 
             var jsonSuppliers = File.ReadAllText("../../../Datasets/suppliers.json");
-            var suppliers = JsonConvert.DeserializeObject<Supplier[]>(jsonSuppliers);
+            Console.WriteLine(ImportSuppliers(context, jsonSuppliers));
+            
+
+            ////Insert Parts Data
+            //var jsonParts = File.ReadAllText("../../../Datasets/parts.json");
+            //var parts = JsonConvert.DeserializeObject<Part[]>(jsonParts);
+            //context.Parts.AddRange(parts);
+
+            ////Insert Cars Data
+            //var jsonCars = File.ReadAllText("../../../Datasets/cars.json");
+            //var cars = JsonConvert.DeserializeObject<Car[]>(jsonCars);
+            //context.Cars.AddRange(cars);
+
+            ////Insert Customers Data
+            //var jsonCustomers = File.ReadAllText("../../../Datasets/customers.json");
+            //var customers = JsonConvert.DeserializeObject<Customer[]>(jsonCustomers);
+            //context.Customers.AddRange(customers);
+
+            ////Insert Sales Data
+            //var jsonSales = File.ReadAllText("../../../Datasets/sales.json");
+            //var sales = JsonConvert.DeserializeObject<Sale[]>(jsonSales);
+            //context.Sales.AddRange(sales);
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliers = JsonConvert.DeserializeObject<Supplier[]>(inputJson);
             context.Suppliers.AddRange(suppliers);
-
-            //Insert Parts Data
-            var jsonParts = File.ReadAllText("../../../Datasets/parts.json");
-            var parts = JsonConvert.DeserializeObject<Part[]>(jsonParts);
-            context.Parts.AddRange(parts);
-
-            //Insert Cars Data
-            var jsonCars = File.ReadAllText("../../../Datasets/cars.json");
-            var cars = JsonConvert.DeserializeObject<Car[]>(jsonCars);
-            context.Cars.AddRange(cars);
-
-            //Insert Customers Data
-            var jsonCustomers = File.ReadAllText("../../../Datasets/customers.json");
-            var customers = JsonConvert.DeserializeObject<Customer[]>(jsonCustomers);
-            context.Customers.AddRange(customers);
-
-            //Insert Sales Data
-            var jsonSales = File.ReadAllText("../../../Datasets/sales.json");
-            var sales = JsonConvert.DeserializeObject<Sale[]>(jsonSales);
-            context.Sales.AddRange(sales);
-
             context.SaveChanges();
+            var result = $"Successfully imported {suppliers.Count()}.";
+            return result;
         }
     }
 }
