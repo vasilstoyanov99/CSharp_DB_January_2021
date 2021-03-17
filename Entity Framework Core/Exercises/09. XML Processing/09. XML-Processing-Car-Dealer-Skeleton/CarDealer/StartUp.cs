4c35bd313cc53a5296247fc6032b891237ab4de7
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-03-17 17:55:00 +0200

    Add done 09. Import Suppliers task

--- Entity Framework Core/Exercises/09. XML Processing/09. XML-Processing-Car-Dealer-Skeleton/CarDealer/StartUp.cs
+++ Entity Framework Core/Exercises/09. XML Processing/09. XML-Processing-Car-Dealer-Skeleton/CarDealer/StartUp.cs
@@ -1,10 +1,39 @@
-﻿namespace CarDealer
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using CarDealer.Data;
+using CarDealer.DataTransferObjects;
+using CarDealer.Models;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
+            var context = new CarDealerContext();
+            var inputXml = File.ReadAllText("./Datasets/suppliers.xml");
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            Console.WriteLine(ImportSuppliers(context, inputXml));
+        }
 
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(SupplierInputModel[]), 
+                new XmlRootAttribute("Suppliers"));
+            var stringReader = new StringReader(inputXml);
+            var suppliersDTOs = xmlSerializer.Deserialize(stringReader) as SupplierInputModel[];
+            var suppliers = suppliersDTOs.Select(x => new Supplier()
+            {
+                Name = x.Name,
+                IsImporter = x.IsImporter
+            })
+                .ToList();
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+            return $"Successfully imported {suppliers.Count}";
         }
     }
 }
