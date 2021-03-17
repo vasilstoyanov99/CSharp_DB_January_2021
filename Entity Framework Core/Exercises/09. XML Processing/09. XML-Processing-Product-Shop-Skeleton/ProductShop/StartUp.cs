e8fff258040bd239877f4c7aea95adfaf7f697da
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-03-17 17:14:21 +0200

    Add XML exercise skeletons

---
+++ Entity Framework Core/Exercises/09. XML Processing/09. XML-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using ProductShop.Data;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+
+        }
+    }
+}
