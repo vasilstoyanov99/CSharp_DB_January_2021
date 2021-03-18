76b909cf0699f90eb4bff5755d55549e251103ce
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-03-18 03:10:18 +0200

    Update StartUp.cs

--- Entity Framework Core/Exercises/09. XML Processing/09. XML-Processing-Car-Dealer-Skeleton/CarDealer/StartUp.cs
+++ Entity Framework Core/Exercises/09. XML Processing/09. XML-Processing-Car-Dealer-Skeleton/CarDealer/StartUp.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Xml.Serialization;
 using AutoMapper;
+
 using CarDealer.Data;
 using CarDealer.DataTransferObjects;
 using CarDealer.Models;
