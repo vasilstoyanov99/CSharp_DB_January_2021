52a6e39c38485e5a95c14599951e4060d6095a1a
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-03-22 16:16:34 +0200

    Add done 01. Import Users task

--- Entity Framework Core/Exercises/09. XML Processing/09. XML-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
+++ Entity Framework Core/Exercises/09. XML Processing/09. XML-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
@@ -1,4 +1,11 @@
-﻿using ProductShop.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using ProductShop.Data;
+using ProductShop.DataTransferObjects.Import;
+using ProductShop.Models;
 
 namespace ProductShop
 {
@@ -7,7 +14,28 @@ namespace ProductShop
         public static void Main(string[] args)
         {
             var context = new ProductShopContext();
+            var json = File.ReadAllText("./Datasets/users.xml");
+            Console.WriteLine(ImportUsers(context, json));
+        }
 
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            const string root = "Users";
+            var xmlSerializer = new XmlSerializer(typeof(List<ImportUsersDTO>),
+                new XmlRootAttribute(root));
+            var stringReader = new StringReader(inputXml);
+            var usersDTO = xmlSerializer.Deserialize(stringReader) as List<ImportUsersDTO>;
+            var users = usersDTO
+                .Select(u => new User
+                {
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Age = u.Age
+                })
+                .ToList();
+            context.Users.AddRange(users);
+            context.SaveChanges();
+            return $"Successfully imported {users.Count}";
         }
     }
 }
