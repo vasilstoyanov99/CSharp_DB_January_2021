1a6a7c9ee6f38cbec84713be9959bddcbe5ce099
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-04-01 19:37:14 +0300

    change folder name on 08 August 2020 exam skeleton and expected result

---
+++ Entity Framework Core/Exam_Preparation/01. Model Definition_Skeleton + Datasets/VaporStore/Data/VaporStoreDbContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using VaporStore.Data.Models;
+
+namespace VaporStore.Data
+{
+	using Microsoft.EntityFrameworkCore;
+
+	public class VaporStoreDbContext : DbContext
+	{
+		public VaporStoreDbContext()
+		{
+		}
+
+		public VaporStoreDbContext(DbContextOptions options)
+			: base(options)
+		{
+		}
+
+        public DbSet<Card> Cards{ get; set; }
+
+        public DbSet<Developer> Developers { get; set; }
+
+        public DbSet<Game> Games { get; set; }
+
+        public DbSet<GameTag> GameTags { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Purchase> Purchases { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			if (!options.IsConfigured)
+			{
+				options
+					.UseSqlServer(Configuration.ConnectionString);
+			}
+		}
+
+		protected override void OnModelCreating(ModelBuilder model)
+        {
+            model.Entity<Purchase>(entity => 
+            {
+                entity.HasKey(ck => new { ck.GameId, ck.CardId});
+
+                entity
+                    .HasOne(x => x.Game)
+                    .WithMany(x => x.Purchases)
+                    .HasForeignKey(x => x.GameId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity
+                    .HasOne(x => x.Card)
+                    .WithMany(x => x.Purchases)
+                    .HasForeignKey(x => x.CardId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
+	}
+}
