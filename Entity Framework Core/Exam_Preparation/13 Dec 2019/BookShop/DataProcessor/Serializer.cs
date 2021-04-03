660df199dee95b9f64622f79b3e728f5419b68e5
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-04-03 19:40:55 +0300

    Add done 03. Data Export task

--- Entity Framework Core/Exam_Preparation/13 Dec 2019/BookShop/DataProcessor/Serializer.cs
+++ Entity Framework Core/Exam_Preparation/13 Dec 2019/BookShop/DataProcessor/Serializer.cs
@@ -1,4 +1,7 @@
-﻿namespace BookShop.DataProcessor
+﻿using BookShop.DataProcessor.ExportDto;
+using XmlFacade;
+
+namespace BookShop.DataProcessor
 {
     using System;
     using System.Globalization;
@@ -15,12 +18,48 @@
     {
         public static string ExportMostCraziestAuthors(BookShopContext context)
         {
-            throw new NotImplementedException();
+            var sortedAuthors = context
+                .Authors
+                .ToList()
+                .Select(a => new
+                {
+                    AuthorName = a.FirstName + " " + a.LastName,
+                    Books = a.AuthorsBooks
+                        .OrderByDescending(b => b.Book.Price)
+                        .Select(b => new
+                        {
+                            BookName = b.Book.Name,
+                            BookPrice = b.Book.Price.ToString("F2")
+                        })
+                        .ToList()
+                })
+                .OrderByDescending(a => a.Books.Count)
+                .ThenBy(a => a.AuthorName)
+                .ToList();
+
+            var jsonResult = JsonConvert.SerializeObject(sortedAuthors, Formatting.Indented);
+            return jsonResult;
         }
 
         public static string ExportOldestBooks(BookShopContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            var sortedBooks = context
+                .Books
+                .ToList()
+                .Where(b => b.PublishedOn < date && b.Genre.ToString() == "Science")
+                .OrderByDescending(b => b.Pages)
+                .ThenBy(b => b.PublishedOn)
+                .Select(b => new OldestBooksExportModel()
+                {
+                    Name = b.Name,
+                    Date = b.PublishedOn.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture),
+                    Pages = b.Pages.ToString()
+                })
+                .Take(10)
+                .ToList();
+
+            var jsonResult = XmlConverter.Serialize(sortedBooks, "Books");
+            return jsonResult;
         }
     }
 }
