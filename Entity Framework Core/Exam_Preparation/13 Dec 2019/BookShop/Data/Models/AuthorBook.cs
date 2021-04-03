6b1e995265b2149822d04a91dcb3bce13b5a93f6
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-04-03 16:18:20 +0300

    Add done 01. Model Definition task

---
+++ Entity Framework Core/Exam_Preparation/13 Dec 2019/BookShop/Data/Models/AuthorBook.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.Data.Models
+{
+    public class AuthorBook
+    {
+        [Required]
+        public int AuthorId { get; set; }
+
+        public Author Author { get; set; }
+
+        [Required]
+        public int BookId { get; set; }
+
+        public Book Book { get; set; }
+    }
+}
---
+++ Entity Framework Core/Exam_Preparation/13 Dec 2019/BookShop/Data/Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using BookShop.Data.Models.Enums;
+
+namespace BookShop.Data.Models
+{
+    public class Book
+    {
+        public Book()
+        {
+            AuthorsBooks = new HashSet<AuthorBook>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Pages { get; set; }
+
+        [Required]
+        public DateTime PublishedOn { get; set; }
+
+        public ICollection<AuthorBook> AuthorsBooks { get; set; }
+    }
+}
